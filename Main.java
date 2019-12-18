8fc87424a0dc4f30fb4820981da20a2452dc62c6
spandey1296 <50301680+spandey1296@users.noreply.github.com>
2019-12-18 19:05:59 +0530

    web

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.io.IOException;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Runtime rs = Runtime.getRuntime();
+        try {
+
+
+            rs.exec("notepad");
+        } catch (IOException e) {
+
+            System.out.println(e);
+
+
+        }
+
+    }
+}
